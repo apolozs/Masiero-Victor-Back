17e637924325fbf59638d12ea43ddb7d82dc8901
Perez <victor.perezsilva@hotmail.com>
2021-09-29 18:44:49 -0300

    Correção do cors e criação de testes

--- AndreyPerez/Startup.cs
+++ AndreyPerez/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AndreyPerez.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +29,20 @@ namespace AndreyPerez
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(
+                options => {
+                    options.AddPolicy("CorsPolicy", builder => builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+                }
+            );
 
+
+            services.AddDbContext<DataContext>
+            (
+                options => options.UseInMemoryDatabase("database")
+            );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -46,6 +61,8 @@ namespace AndreyPerez
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AndreyPerez v1"));
             }
 
+            app.UseCors("CorsPolicy");
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
